2b2fcc88f8508f0efc7b08e8754ca03b79762f82
orville94bailey <orville94bailey@gmail.com>
2019-07-05 23:48:34 -0400

    win check is working move prevention is working

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,6 +2,9 @@ let playerTurn = 0;
 let lastRow = '';
 let lastColumn = '';
 
+let board = Array.from(Array(9), ()=> new Array(9))
+let largeBoard = Array.from(Array(3), ()=> new Array(3));
+
 function setup() {
     createCanvas(300, 300);
 }
@@ -49,15 +52,20 @@ function drawX(x,y) {
 function mouseClicked() {
     let outerCoords = getQuadrant()
     let innerCoords = getInnerQuadrant()
-    let x = (outerCoords.x * 100) + (innerCoords.x * 33) + 15
-    let y = (outerCoords.y * 100) + (innerCoords.y * 33) + 15
-    console.log(x,'   ',y)
-    if (playerTurn % 2 ===1) {
-        drawCircle(x,y)
-    } else {
-        drawX(x,y)
+    if (isValidMove(outerCoords, innerCoords)) {
+        let x = (outerCoords.x * 100) + (innerCoords.x * 33) + 15
+        let y = (outerCoords.y * 100) + (innerCoords.y * 33) + 15 
+        if (playerTurn % 2 ===1) {
+            drawCircle(x,y)
+            markBoard(outerCoords, innerCoords, 'o')
+        } else {
+            drawX(x,y)
+            markBoard(outerCoords,innerCoords,'x')
+        }
+        let player = playerTurn % 2 == 1 ? 'o' : 'x';
+        checkForWinner(outerCoords,player)
+        playerTurn += 1;   
     }
-    playerTurn += 1;
     return false;
 }
 
@@ -68,3 +76,48 @@ function getQuadrant() {
 function getInnerQuadrant() {
     return {x:Math.floor((mouseX%100)/33), y:Math.floor((mouseY%100)/33)}
 }
+
+function markBoard(outerCoords, innerCoords, player) {
+    board[(outerCoords.x * 3) + innerCoords.x][(outerCoords.y * 3)+innerCoords.y] = player;
+    console.log(board);
+}
+
+function checkForWinner(outerCoords, player) {
+    var result;
+    for(let y = (outerCoords.y*3); y <=(outerCoords.y*3)+2; y++) {
+        //top to bottom
+        for (let x=(outerCoords.x*3);x<=(outerCoords.x*3)+2;x++) {
+            //left to right
+            if (board[x][y] == player) {
+                // console.log(x%3+y%3, '=',x%3,'+',y%3)
+                result = result | (1<<((x%3)+((y%3)*3)))
+            }
+        }
+    }
+    if (winningValues.indexOf(result) != -1) {
+        largeBoard[outerCoords.x][outerCoords.y] = player;
+    }
+}
+
+function isValidMove(outerCoords, innerCoords){
+    console.log(largeBoard[outerCoords.x][outerCoords.y])
+    console.log(board[innerCoords.x][innerCoords.y])
+
+    if (largeBoard[outerCoords.x][outerCoords.y] == undefined) {
+        if (board[(outerCoords.x * 3) + innerCoords.x][(outerCoords.y * 3)+innerCoords.y] == undefined) {
+            return true;
+        }
+    }
+    return false;
+}
+
+let winningValues = [
+    parseInt("100100100",2),
+    parseInt("010010010",2),
+    parseInt("001001001",2),
+    parseInt("111000000",2),
+    parseInt("000111000",2),
+    parseInt("000000111",2),
+    parseInt("100010001",2),
+    parseInt("001010100",2)
+]
